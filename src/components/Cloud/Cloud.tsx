41648299ce2ecd3d4f8211fa64aaa326887d0460
Keturah Howard <keturah.dev@gmail.com>
2023-10-26 16:47:37 -0700

    add form to pass in user mantra to rain function

--- src/components/Cloud/Cloud.tsx
+++ src/components/Cloud/Cloud.tsx
@@ -1,12 +1,13 @@
+import { useState } from 'react';
 import styles from './Cloud.module.css';
 
 const Cloud = () => {
-  
+  const [hideForm, setHideForm] = useState<boolean>(false);
   const getLetter = (input: string = "love") => {
     return input[Math.floor(Math.random() * input.length)];
   }
 
-  const rain = () => {
+  const rain = (mantra: string) => {
     let cloud = document.querySelector('#cloud');
     let e = document.createElement('div');
     let left = Math.floor(Math.random() * 320);
@@ -15,7 +16,7 @@ const Cloud = () => {
 
     e.classList.add(styles.text);
     cloud?.appendChild(e);
-    e.innerText = getLetter();
+    e.innerText = getLetter(mantra);
     e.style.left = left + "px";
     e.style.fontSize = 0.5 * size + 'em';
 
@@ -25,14 +26,27 @@ const Cloud = () => {
     }, 600);
   };
 
-  setInterval(() => {
-    rain();
-  }, 100);
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      mantra: { value: string };
+    };
+    console.log(target.mantra.value);
+    setHideForm(true);
+    setInterval(() => {
+      rain(target.mantra.value);
+    }, 100);
+  }
 
   return(
-    <div className={styles.container}>
-      <div id="cloud" className={styles.cloud}>
+    <div>
+      <div className={styles.container}>
+        <div id="cloud" className={styles.cloud}/>
       </div>
+      <form onSubmit={handleSubmit} className={`${styles.formContainer} ${hideForm ? styles.hidden : null}`} name="submit mantra">
+        <input name="mantra" type='text'/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 };
